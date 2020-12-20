08be28abe6c080d6eb49954d938574d7ec96814f
arunravichandran-11 <arunravichandran11@gmail.com>
2020-12-20 19:46:03 +0530

    Scripts for publishing to npm is also added

---
+++ npm_publish.sh
@@ -0,0 +1,44 @@
+# establish branch and tag name variables
+devBranch=develop
+
+
+# current Git branch
+branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+tagVersion=v$1;
+echo $branch $tagVersion
+
+function publish_to_npm {
+
+    if [ -z "$tagVersion" ]
+    then
+        echo "Please switch to corresponding tag before publishing to npm"
+    else
+
+    # git describe --exact-match --tags $(git log -n1 --pretty='%h')
+    # git describe --tags
+
+    # checkout to tag
+    git checkout $tagVersion
+    
+    # Now latest tag version is switched and new code is there. So Switch to doc-site
+    cd ./docs-site/
+
+    # Run command to create lib folder:
+    npm run ready
+    cd ..
+
+    # publish the code to npm.
+    npm publish
+
+    # After publishing , move to develop branch.
+    git checkout develop
+
+}
+
+if [ $branch == $devBranch ]
+then
+   publish_to_npm
+else
+   echo "Please switch to develop branch"
+
+fi
