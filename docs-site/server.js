9ddf00bb3349630c318b72ca7941b1474d1b64bb
arunravichandran-11 <arunravichandran11@gmail.com>
2021-05-27 14:01:25 +0530

    test this Signed-off-by: arunravichandran-11 <arunravichandran11@gmail.com>

--- docs-site/server.js
+++ docs-site/server.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const { func } = require('prop-types');
-const app = express();
-
-app.use(express.static('dist'));
-app.get('*', (req, res) => {
-    res.sendFile('./dist/index.html', { root: __dirname });
-});
-
-app.listen(3200, function(req, res) {
-    console.log('app served');
-});
+// const express = require('express');
+// const { func } = require('prop-types');
+// const app = express();
+//
+// app.use(express.static('dist'));
+// app.get('*', (req, res) => {
+//     res.sendFile('./dist/index.html', { root: __dirname });
+// });
+// app.listen(3200, function(req, res) {
+//     console.log('app served');
+// });
