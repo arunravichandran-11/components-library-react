65d9fa0b753206b2c9bc3162c53920090cf2aa4b
arunravichandran-11 <arunravichandran11@gmail.com>
2021-05-27 15:12:24 +0530

    Task/COREAR-12323-tkjbw wrkbgkew ejgjbewkbg

--- docs-site/server.js
+++ docs-site/server.js
@@ -5,6 +5,6 @@ app.use(express.static('dist'));
 app.get('*', (req, res) => {
     res.sendFile('./dist/index.html', { root: __dirname });
 });
-// app.listen(3200, function(req, res) {
-//     console.log('app served');
-// });
+app.listen(3200, function(req, res) {
+    console.log('app served');
+});
