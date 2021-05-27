a3d04231343846469f6be4a43f0ad3a5a022a369
arunravichandran-11 <arunravichandran11@gmail.com>
2021-05-27 14:19:50 +0530

    COREAR-2-testmsg

--- docs-site/server.js
+++ docs-site/server.js
@@ -1,6 +1,6 @@
-// const express = require('express');
-// const { func } = require('prop-types');
-// const app = express();
+const express = require('express');
+const { func } = require('prop-types');
+const app = express();
 //
 // app.use(express.static('dist'));
 // app.get('*', (req, res) => {
