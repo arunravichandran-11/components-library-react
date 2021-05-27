1a1620f6654a69299970f0d4184e651bed0c4726
arunravichandran-11 <arunravichandran11@gmail.com>
2021-05-27 13:58:52 +0530

    test

--- docs-site/server.js
+++ docs-site/server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const { func } = require('prop-types');
 const app = express();
 
-app.use(express.static('dist'))
-
+app.use(express.static('dist'));
 app.get('*', (req, res) => {
     res.sendFile('./dist/index.html', { root: __dirname });
 });
