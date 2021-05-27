08e8e4a2adcd921c3a46b7548f2a05003322eacd
arunravichandran-11 <arunravichandran11@gmail.com>
2021-05-27 14:32:56 +0530

    COREAR-223-testmsg

--- docs-site/server.js
+++ docs-site/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { func } = require('prop-types');
 const app = express();
-
 app.use(express.static('dist'));
 app.get('*', (req, res) => {
     res.sendFile('./dist/index.html', { root: __dirname });
